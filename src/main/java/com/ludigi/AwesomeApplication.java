bb57f4874f6bd5c83425131103d117c10d1bcb82
Sławek Ludwiczak <slawek.ludwiczak@gmail.com>
2024-08-30 18:53:03 +0200

    Add application starting point
    Closes: #1

---
+++ src/main/java/com/ludigi/AwesomeApplication.java
@@ -0,0 +1,9 @@
+package com.ludigi;
+
+public class AwesomeApplication {
+    private final static String APPLICATION_VERSION = "1.0.0";
+
+    public static void main(String[] args) {
+        System.out.printf("Awesome application (ver. %s)", APPLICATION_VERSION);
+    }
+}
