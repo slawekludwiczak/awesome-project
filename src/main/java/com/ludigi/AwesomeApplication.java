52063bd279bbcaba437aae2f309ceaf8b7a6a64b
Sławek Ludwiczak <slawek.ludwiczak@gmail.com>
2024-09-02 14:55:43 +0200

    Print current time after application version
    Closes #2

--- src/main/java/com/ludigi/AwesomeApplication.java
+++ src/main/java/com/ludigi/AwesomeApplication.java
@@ -1,9 +1,15 @@
 package com.ludigi;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
+
 public class AwesomeApplication {
     private final static String APPLICATION_VERSION = "1.0.0";
+    static Clock clock = Clock.systemDefaultZone();
 
     public static void main(String[] args) {
-        System.out.printf("Awesome application (ver. %s)", APPLICATION_VERSION);
+        System.out.printf("Awesome application (ver. %s)\n", APPLICATION_VERSION);
+        LocalDateTime now = LocalDateTime.now(clock);
+        System.out.println("started at: " + now);
     }
 }
