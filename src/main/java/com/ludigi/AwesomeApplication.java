e3447a26c49a0a088499601073461e407a184046
Sławek Ludwiczak <slawek.ludwiczak@gmail.com>
2024-09-06 09:22:30 +0200

    add checkstyle, pmd, sonar

--- src/main/java/com/ludigi/AwesomeApplication.java
+++ src/main/java/com/ludigi/AwesomeApplication.java
@@ -4,12 +4,19 @@ import java.time.Clock;
 import java.time.LocalDateTime;
 
 public class AwesomeApplication {
-    private final static String APPLICATION_VERSION = "1.0.0";
+    private static final String APPLICATION_VERSION = "1.0.0";
     static Clock clock = Clock.systemDefaultZone();
 
     public static void main(String[] args) {
         System.out.printf("Awesome application (ver. %s)\n", APPLICATION_VERSION);
         LocalDateTime now = LocalDateTime.now(clock);
         System.out.println("started at: " + now);
+        int x = 5;
+        emptyMethod();
     }
+
+    static void emptyMethod() {
+
+    }
+
 }
